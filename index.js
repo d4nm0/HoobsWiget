30b31c224127559afa1d9222ad65147cec21b534
Dan Monceau <dan.monceau.lpcb@gmail.com>
2022-08-15 01:57:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Dan Monceau
+// Code Version: 15/08/2022
+// ******
+// the widget was created in - 1 day. if you find any bugs, send me a message on LinkedIn or by email.
+// https://www.linkedin.com/in/dan-monceau-b8b56a131
+// @ : dan.monceau.lpcb@gmail.com
+
 const maxLineWidth = 600; 
 const normalLineHeight = 35;
 
@@ -12,10 +19,10 @@ Script.complete();
 
 async function createWidget() {
   
+  // connecting to Hoobs for get data
   const headers = {
       'accept': '*\/*', 'Content-Type': 'application/json'
   };  
-  
   let req = new Request('http://hoobs.local/api/auth/logon');
   req.method = 'POST';
   req.headers = headers;
@@ -34,44 +41,38 @@ async function createWidget() {
   }
     
   let listwidget = new ListWidget();
-  
   listwidget.setPadding(10, 10, 10, 10)
   
-  let nextRefresh = Date.now() + 1000*5 // add 30 second to now
-	
+  // Auto Refresh 
+  let nextRefresh = Date.now() + 1000*5 
   listwidget.refreshAfterDate = new Date(nextRefresh)
   
+  // Generate title with Image
   let img = await new Request("https://github.com/d4nm0/HoobsWiget/blob/main/hoobs.png?raw=true").loadImage()
-  
   let imgstack = listwidget.addImage(img)
   let title = listwidget.addText("Hoobs") 
   title.font = Font.boldSystemFont(16) 
-  
-  
   imgstack.imageSize = new Size(40,40)
   title.centerAlignText();
   imgstack.centerAlignImage()
   listwidget.addSpacer(20)
   
+  
   let mainStack = listwidget.addStack() 
   
-  
+  // generate left stack
   let leftStack = mainStack.addStack() 
   leftStack.layoutVertically() 
-  
   let cpuLoad = await getCpuLoad(leftStack, token)
-  //console.log(toto)
   leftStack.addText('CPU Load : ' + cpuLoad.toString() + '%')
-  
   let memoryUsage = await getMemory(leftStack, token)
   leftStack.addText('RAM Usage : ' + memoryUsage.toString() + '%')
   mainStack.addSpacer(30) 
   
+  // generate right stack
   let rightStack = mainStack.addStack() 
-  
   let cputemp = await getCpuTemp(rightStack, token)
   rightStack.addText('CPU Temp : ' + cputemp.toString() + '°C');
-  
   rightStack.layoutVertically() 
   let running = await getStatus(rightStack, token)
   if (running = true) {
@@ -80,17 +81,15 @@ async function createWidget() {
     rightStack.addText('ko' + ' Running') 
   }
   
-  
+  // generate bottom text
   let dateTime = new Date()
   let df = new DateFormatter()
   df.locale = 'fr'
   df.useFullDateStyle()
   let formattedString = df.string(dateTime)
-
   let sub = listwidget.addText("Updated : " + formattedString)
   sub.font = Font.systemFont(10)
   sub.centerAlignText();
-  //test(listwidget);
   
   return listwidget;
   
@@ -103,17 +102,15 @@ async function getCpuLoad(stack, token){
       'Authorization': token
   };
   let req = new Request('http://hoobs.local/api/system/cpu');
-    
   req.method = 'GET';
   req.headers = headers;
   let result;
   try {
      result = await req.loadJSON();
-     
   } catch (e) {
      return null;
   }
-  //console.log(result.load.currentLoad)
+  
   let cpuLoad = parseInt(result.load.currentLoad)
   return cpuLoad
 }
@@ -125,7 +122,6 @@ async function getMemory(stack, token){
       'Authorization': token
   };
   let req = new Request('http://hoobs.local/api/system/memory');
-    
   req.method = 'GET';
   req.headers = headers;
   let result;
@@ -135,7 +131,7 @@ async function getMemory(stack, token){
   } catch (e) {
      return null;
   }
-  console.log((result.load.active /result.load.available) * 100)
+  
   let purcentUseMemory = parseInt((result.load.active /result.load.available) * 100)
   return purcentUseMemory
 }
@@ -147,41 +143,34 @@ async function getCpuTemp(stack, token){
       'Authorization': token
   };
   let req = new Request('http://hoobs.local/api/system/temp');
-    
   req.method = 'GET';
   req.headers = headers;
   let result;
   try {
      result = await req.loadJSON();
-     console.log(result.max)
   } catch (e) {
      return null;
   }
+  
   let cpuTemp = parseInt(result.max)
-  //console.log(result.load.currentLoad)
   return cpuTemp
 }
 
 
 async function getStatus(stack, token){
-  //let string = 'CPU Temp : '
-  
   let headers = {
       'Authorization': token
   };
   let req = new Request('http://hoobs.local/api/status');
-    
   req.method = 'GET';
   req.headers = headers;
   let result;
   try {
      result = await req.loadJSON();
-     console.log(result.bridges.hoobs.running)
   } catch (e) {
      return null;
   }
-  //let cpuTemp = parseInt(result.max)
-  //console.log(result.load.currentLoad)
+  
   return result.bridges.hoobs.running
 }
 
